51f369611572fc74727402168871f970aa2f6b19
Abhishek <ab.umarjikar@gmail.com>
2018-07-05 16:29:32 +0530

    practice code added

--- Practice/src/com/ab/test/Test.java
+++ Practice/src/com/ab/test/Test.java
@@ -15,6 +15,19 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("This is Test For Git...");
 
+		String testString = "This is Test For Git...";
+		char[] charArray = testString.toCharArray();
+		StringBuilder sb = new StringBuilder();
+		if (charArray != null && charArray.length > 0) {
+
+			for (char ch : charArray) {
+				int ascii = (int) ch;
+				if (ascii >= 65 && ascii < 97)
+					ascii += 32;
+				sb.append((char) ascii);
+			}
+		}
+		System.out.println(sb.toString());
 	}
 
 }
